a3ec964df8233bd9fa742a2fac492f32ce026f1a
kidehae <149243635+kidehae@users.noreply.github.com>
2025-05-19 06:14:54 +0300

    Added header and footer

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <div className="App">
-      <h1>wwassap </h1>
+      <Home />
     </div>
   );
 }
---
+++ netflix-clone/src/pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../../componenets/Header/Header";
+import Footer from "../../componenets/Footer/Footer";
+function Home() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
